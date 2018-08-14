9136a9f4c6e3411e6810c285498fb15597b9f523
Daniel Bosen <daniel.bosen@burda.com>
2018-08-14 17:24:09 +0200

    do not test coverage

--- lib/testing-library.sh
+++ lib/testing-library.sh
@@ -171,7 +171,7 @@ run_tests() {
 
     case ${THUNDER_TRAVIS_TEST_RUNNER} in
         "phpunit")
-            php ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/phpunit --verbose --debug -c ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/core ${test_selection} ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/modules/contrib/${THUNDER_TRAVIS_PROJECT_NAME} || exit 1
+            php ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${composer_bin_dir}/phpunit --no-coverage --verbose --debug -c ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/core ${test_selection} ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/modules/contrib/${THUNDER_TRAVIS_PROJECT_NAME} || exit 1
         ;;
         "run-tests")
             php ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/${docroot}/core/scripts/run-tests.sh --php $(which php) --suppress-deprecations --verbose --color --url http://${THUNDER_TRAVIS_HOST}:${THUNDER_TRAVIS_HTTP_PORT} ${THUNDER_TRAVIS_TEST_GROUP} || exit 1
