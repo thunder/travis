34d1daf3ecba3faa5e896972a5b5cb5fa0a759c1
Daniel Bosen <daniel.bosen@burda.com>
2018-08-16 11:16:11 +0200

    remove debug

--- lib/testing-library.sh
+++ lib/testing-library.sh
@@ -175,8 +175,6 @@ _stage_build_project() {
 
     composer require webflo/drupal-core-require-dev:${THUNDER_TRAVIS_DRUPAL_VERSION} --dev --no-update --working-dir=${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
 
-    cat ${THUNDER_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}/composer.json
-
     require_local_project
     composer_install
     move_assets
