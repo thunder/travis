8e7358a40de5dded19d4f3bf2a467790b9981cd7
Daniel Bosen <daniel.bosen@burda.com>
2019-03-18 12:38:37 +0100

    MOve neon file to project

--- lib/stages/run_tests.sh
+++ lib/stages/run_tests.sh
@@ -12,6 +12,7 @@ _stage_run_tests() {
 
     if ${DRUPAL_TRAVIS_TEST_DEPRECATION} && [[ -f phpstan.neon ]]; then
         printf "Checking for deprecations.\n\n"
+        cp phpstan.neon ${DRUPAL_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
         cd ${DRUPAL_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
         phpstan analyse --memory-limit 300M ${project_location}
         cd -
