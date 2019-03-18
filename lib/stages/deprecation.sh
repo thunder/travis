ea2bbe8afa7e4a783963fcb65053fa1f52fe9797
Daniel Bosen <daniel.bosen@burda.com>
2019-03-18 14:09:43 +0100

    Move phpstan.neon test

--- lib/stages/deprecation.sh
+++ lib/stages/deprecation.sh
@@ -5,7 +5,7 @@ _stage_deprecation() {
 
     local project_location=$(get_project_location)
 
-    if ${DRUPAL_TRAVIS_TEST_DEPRECATION} && [[ -f phpstan.neon ]]; then
+    if ${DRUPAL_TRAVIS_TEST_DEPRECATION}; then
         cp phpstan.neon ${DRUPAL_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
         cd ${DRUPAL_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
         vendor/bin/phpstan analyse --memory-limit 300M ${project_location}
