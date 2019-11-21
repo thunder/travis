7a17c04aa63d1d42d91e6eb7c8f7e7a236392e26
Daniel Bosen <daniel.bosen@burda.com>
2019-11-21 13:25:09 +0100

    continued debugging

--- lib/stages/deprecation.sh
+++ lib/stages/deprecation.sh
@@ -21,32 +21,35 @@ _stage_deprecation() {
         pushd "${DRUPAL_TRAVIS_TEST_BASE_DIRECTORY}"
         composer create-project drupal-composer/drupal-project:8.x-dev "${installation_directory}" --stability dev --no-interaction --no-install
 
+        cd "${installation_directory}" || exit
+
         # Add asset-packagist for projects, that require frontend assets
-        composer config repositories.assets composer https://asset-packagist.org --working-dir="${installation_directory}"
+        composer config repositories.assets composer https://asset-packagist.org
 
         # Require the specific Drupal core version we need, as well as the corresponding dev-requirements
-        composer require drupal/core:"${DRUPAL_TRAVIS_TEST_DEPRECATION_DRUPAL_CHECK_DRUPAL_VERSION}" --no-update --working-dir="${installation_directory}"
-        composer require webflo/drupal-core-require-dev:"${DRUPAL_TRAVIS_TEST_DEPRECATION_DRUPAL_CHECK_DRUPAL_VERSION}" --dev --no-update --working-dir="${installation_directory}"
+        composer require drupal/core:"${DRUPAL_TRAVIS_TEST_DEPRECATION_DRUPAL_CHECK_DRUPAL_VERSION}" --no-update
+        composer require webflo/drupal-core-require-dev:"${DRUPAL_TRAVIS_TEST_DEPRECATION_DRUPAL_CHECK_DRUPAL_VERSION}" --dev --no-update
 
         # Add the local instance of the project into the repositories section and make sure it is used first.
-        composer config repositories.0 path "${DRUPAL_TRAVIS_PROJECT_BASEDIR}" --working-dir="${installation_directory}"
-        composer config repositories.1 composer https://packages.drupal.org/8 --working-dir="${installation_directory}"
+        composer config repositories.0 path "${DRUPAL_TRAVIS_PROJECT_BASEDIR}"
+        composer config repositories.1 composer https://packages.drupal.org/8
 
         # Enable patching
-        composer config extra.enable-patching true --working-dir="${installation_directory}"
+        composer config extra.enable-patching true
 
         # require the project, we want to test.
-        composer require "${DRUPAL_TRAVIS_COMPOSER_NAME}" *@dev --no-update --working-dir="${installation_directory}"
+        composer require "${DRUPAL_TRAVIS_COMPOSER_NAME}" *@dev --no-update
 
         # Use jq to find all dev dependencies of the project and add them to root composer file.
         for dev_dependency in $(jq -r  '.["require-dev"?] | keys[] as $k | "\($k):\(.[$k])"' "${DRUPAL_TRAVIS_PROJECT_BASEDIR}"/composer.json); do
-            composer require "${dev_dependency}" --dev --no-update --working-dir="${installation_directory}"
+            composer require "${dev_dependency}" --dev --no-update
         done
 
-        composer require mglaman/drupal-check --no-update --working-dir="${installation_directory}"
+        composer require mglaman/drupal-check --no-update
 
-        cd "${installation_directory}" || exit
+        pwd
 
+        echo "Install drupal.ckeck instance"
         COMPOSER_MEMORY_LIMIT=-1 composer install
         vendor/bin/drupal-check web/"${project_location}"
 
