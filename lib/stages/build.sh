2cb3f34b5d1ed7615526b64aab3917db1af12ec1
Daniel Bosen <daniel.bosen@burda.com>
2019-12-09 11:47:14 +0100

    debug

--- lib/stages/build.sh
+++ lib/stages/build.sh
@@ -4,12 +4,18 @@
 _stage_build() {
     printf "Building the project.\n\n"
 
+    local drupal_version
     local docroot=$(get_distribution_docroot)
     local libraries=${docroot}/libraries;
 
     # Install all dependencies
     cd ${DRUPAL_TRAVIS_DRUPAL_INSTALLATION_DIRECTORY}
     COMPOSER_MEMORY_LIMIT=-1 composer install
+
+    drupal_version=$(composer show 'drupal/core' | grep 'versions' | grep -o -E '[^ ]+$')
+
+    echo "${drupal_version}"
+    exit 1
 
     # Make sure, we have drupal scaffold files. Composer install should have taken care of it, but this fails for
     # Drupal version < 8.8. This should be removed, once 8.7 support is gone.
